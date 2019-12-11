9f5da9ca6f877d89c2ed2ae8bd4bc51cc60cf55a
Sullivan Berger <sullivan.berger@gmail.com>
2019-12-11 13:45:56 +0100

    Creates posts admin views

---
+++ app/Views/admin/posts/index.php
@@ -0,0 +1,32 @@
+<h1>Interface admin</h1>
+
+<h2>Administrer les articles</h2>
+
+<p><a href="?p=admin.posts.add" class="">Ajouter</a></p>
+
+<table class="table">
+
+    <thead>
+        <tr>
+            <td>ID</td>
+            <td>Titre</td>
+            <td>Actions</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($posts as $post): ?>
+
+            <td><?= $post->id; ?></td>
+            <td><?= $post->titre; ?></td>
+            <td>
+                <a href="?p=admin.posts.edit&id=<?= $post->id; ?>" class="">Editer</a>
+                <form action="?p=admin.posts.delete" method="post" style="display: inline;">
+                    <input type="hidden" name="id" value="<?= $post->id; ?>">
+                    <button type="submit" class="">Supprimer</button>
+                </form>
+            </td>
+
+        <?php endforeach; ?>
+    </tbody>
+
+</table>
