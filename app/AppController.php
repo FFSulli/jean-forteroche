14ab4563450243b920c91da58fd70fc1ee1b49b4
Sullivan Berger <sullivan.berger@gmail.com>
2019-12-11 13:11:57 +0100

    Creates the AppController class

---
+++ app/AppController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use \App;
+use Core\Controller\Controller;
+
+/**
+ * Classe AppController
+ */
+
+class AppController extends Controller {
+
+    protected $template = 'default';
+
+    /**
+     * Constructeur de la classe AppController
+     */
+
+    public function __construct() {
+
+        $this->viewPath = ROOT . '/app/Views/';
+
+    }
+
+    /**
+     * Fonction permettant de générer un modèle
+     *
+     * @param string $model_name
+     */
+    
+    public function loadModel($model_name) {
+
+        $this->$model_name = App::getInstance()->getTable($model_name);
+
+    }
+
+}
