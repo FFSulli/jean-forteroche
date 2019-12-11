e41beb8ac5cd4b32609dc82cc2284d960d6e6531
Sullivan Berger <sullivan.berger@gmail.com>
2019-12-11 12:17:34 +0100

    Creates the autoloader class

---
+++ core/Autoloader.php
@@ -0,0 +1,31 @@
+<?php
+namespace Core;
+
+/**
+ * Class Autoloader
+ */
+
+class Autoloader{
+
+    /**
+     * Fonction permettant d'enregistret l'autoloader
+     */
+    
+    static function register(){
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+    }
+
+    /**
+     * Fonction permettant d'inclure le fichier correspondant à la classe
+     * @param $class
+     */
+
+    static function autoload($class){
+        if (strpos($class, __NAMESPACE__ . '\\') === 0){
+            $class = str_replace(__NAMESPACE__ . '\\', '', $class);
+            $class = str_replace('\\', '/', $class);
+            require __DIR__ . '/' . $class . '.php';
+        }
+    }
+
+}
