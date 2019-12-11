4f81e7a30f25223136177f8c2267a5355118a65d
Sullivan Berger <sullivan.berger@gmail.com>
2019-12-11 11:46:02 +0100

    Sets up database informations

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * Informations de connexion à la base de données 
+ */
+
+return array(
+
+    "db_name" => "jean-forteroche",
+    "db_user" => "root",
+    "db_pass" => "root",
+    "db_host" => "localhost"
+    
+);
