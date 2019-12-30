c9c606ad45806b40a4022baeab511f9dd68ec216
Sullivan Berger <sullivan.berger@gmail.com>
2019-12-30 12:50:03 +0100

    Edits the form

--- app/Views/users/login.php
+++ app/Views/users/login.php
@@ -5,7 +5,9 @@
 <?php endif; ?>
 
 <form method="post">
-    <?= $form->input('username', 'Pseudo'); ?>
-    <?= $form->input('password', 'Mot de passe', ['type' => 'password']); ?>
+    <input type="text" name="username" placeholder="Nom d'utilisateur">
+    <input type="password" name="password" placeholder="Mot de passe">
     <button class="btn btn-primary">Envoyer</button>
 </form>
+
+<a href="index.php?p=users.register">Vous n'avez pas de compte ? Inscrivez-vous</a>
