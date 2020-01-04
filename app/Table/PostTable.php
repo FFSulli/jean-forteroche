4c01da41f573697e50005b4b11216547d65666c7
Sullivan Berger <sullivan.berger@gmail.com>
2020-01-04 23:08:50 +0100

    Adds lastThree & first methods

--- app/Table/PostTable.php
+++ app/Table/PostTable.php
@@ -11,6 +11,10 @@ use Core\Table\Table;
 class PostTable extends Table {
 
     protected $table = 'posts';
+
+    public function first() {
+        return $this->query('SELECT posts.id, posts.title, posts.content, posts.excerpt, posts.date FROM posts ORDER BY posts.id ASC');
+    }
     
     /**
      * Récupère les derniers articles
@@ -19,7 +23,11 @@ class PostTable extends Table {
      */
 
     public function last() {
-        return $this->query('SELECT posts.id, posts.title, posts.content, posts.date FROM posts ORDER BY posts.date DESC');
+        return $this->query('SELECT posts.id, posts.title, posts.content, posts.excerpt, posts.date FROM posts ORDER BY posts.id DESC');
+    }
+
+    public function lastThree() {
+        return $this->query('SELECT posts.id, posts.title, posts.content, posts.excerpt, posts.date FROM posts ORDER BY posts.id DESC LIMIT 3');
     }
 
 }
