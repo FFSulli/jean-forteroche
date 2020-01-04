df8875da33a2cd2c97e735a98c549bedaef1ea99
Sullivan Berger <sullivan.berger@gmail.com>
2020-01-04 23:10:38 +0100

    Adds CSS selectors

--- app/Views/posts/chapters.php
+++ app/Views/posts/chapters.php
@@ -1,6 +1,12 @@
-<?php foreach($posts as $post): ?>
-
-    <h2><a href="<?= $post->url ?>"><?= $post->title; ?></a></h2>
-    <p>Extrait</p>
-
-<?php endforeach; ?>
+<a href="index.php" class="link__home">Retour à l'accueil</a>
+<section class="section__chapters-container">
+    <div class="div__chapters-list">
+        <?php foreach($posts as $post): ?>
+            <div class="div__chapter">
+                <h2 class="div__chapter-title"><a href="<?= $post->url ?>" class="div__chapter-link"><?= $post->title; ?></a></h2>
+                <p class="div__chapter-date">Paru le <?= $post->date ?></p>
+                <p class="div__chapter-excerpt"><?= $post->excerpt ?></p>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</section>
