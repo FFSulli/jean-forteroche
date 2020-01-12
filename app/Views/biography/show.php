dc0140e1d9f99803b8a2a1d5415de7e8158cfce8
Sullivan Berger <sullivan.berger@gmail.com>
2020-01-12 13:51:56 +0100

    Adds the Biography view

---
+++ app/Views/biography/show.php
@@ -0,0 +1,28 @@
+<section class="section__biography-container">
+    <div class="div__biography-gallery">
+        <img src="assets/images/jean-forteroche.jpg" alt="Man in a glacier" class="div__biography-image">
+    </div>
+    <div class="div__biography-text">
+        <h2 class="div__biography-title">Biography</h2>
+        <p class="div__biography-p">Dr. Zoidberg, that doesn’t make sense. But, okay! Daddy Bender, we’re hungry. It’s just like the story of the grasshopper and the octopus.
+        All year long, the grasshopper kept burying acorns for winter, while the octopus mooched off his girlfriend and watched TV.
+        But then the winter came, and the grasshopper died, and the octopus ate all his acorns. Also he got a race car.
+        Is any of this getting through to you?</p>
+        <p class="div__biography-p">Dr. Zoidberg, that doesn’t make sense. But, okay! Daddy Bender, we’re hungry. It’s just like the story of the grasshopper and the octopus.
+        All year long, the grasshopper kept burying acorns for winter, while the octopus mooched off his girlfriend and watched TV.
+        But then the winter came, and the grasshopper died, and the octopus ate all his acorns. Also he got a race car.
+        Is any of this getting through to you?</p>
+        <p class="div__biography-p">Dr. Zoidberg, that doesn’t make sense. But, okay! Daddy Bender, we’re hungry. It’s just like the story of the grasshopper and the octopus.
+        All year long, the grasshopper kept burying acorns for winter, while the octopus mooched off his girlfriend and watched TV.
+        But then the winter came, and the grasshopper died, and the octopus ate all his acorns. Also he got a race car.
+        Is any of this getting through to you?</p>
+        <p class="div__biography-p">Dr. Zoidberg, that doesn’t make sense. But, okay! Daddy Bender, we’re hungry. It’s just like the story of the grasshopper and the octopus.
+        All year long, the grasshopper kept burying acorns for winter, while the octopus mooched off his girlfriend and watched TV.
+        But then the winter came, and the grasshopper died, and the octopus ate all his acorns. Also he got a race car.
+        Is any of this getting through to you?</p>
+        <p class="div__biography-p">Dr. Zoidberg, that doesn’t make sense. But, okay! Daddy Bender, we’re hungry. It’s just like the story of the grasshopper and the octopus.
+        All year long, the grasshopper kept burying acorns for winter, while the octopus mooched off his girlfriend and watched TV.
+        But then the winter came, and the grasshopper died, and the octopus ate all his acorns. Also he got a race car.
+        Is any of this getting through to you?</p>
+    </div>
+</section>
