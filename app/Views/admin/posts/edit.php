d3712246215c464169e4340adaf3c67b1f911be0
Sullivan Berger <sullivan.berger@gmail.com>
2020-01-12 13:50:32 +0100

    Updates form to enable TinyMCE

--- app/Views/admin/posts/edit.php
+++ app/Views/admin/posts/edit.php
@@ -1,6 +1,6 @@
 <a href="index.php?p=admin.posts.index" class="admin__dashboard-link">Revenir au Tableau de bord</a>
 <form method="post" class="form__post-edit">
     <input type="text" name="title" placeholder="Titre de l'article">
-    <input type="textarea" name="content" placeholder="Contenu">
+    <textarea name="test" id="test" cols="30" rows="10"></textarea>
     <button class="btn btn-primary">Sauvegarder</button>
 </form>
