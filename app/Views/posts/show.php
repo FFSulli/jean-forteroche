fe9b7551c1914af04e2aa65dce977b4393dc0be9
Sullivan Berger <sullivan.berger@gmail.com>
2020-01-12 13:53:54 +0100

    Adds comments to the post view

--- app/Views/posts/show.php
+++ app/Views/posts/show.php
@@ -2,4 +2,21 @@
 
 <p> <?= $article->content; ?></p>
 
-<p><a href="index.php?p=home">Home</a></p>
+<p><a href="index.php">Home</a></p>
+
+<form method="post" class="form__post-edit">
+    <input type="text" name="comment_author" placeholder="Nom d'utilisateur">
+    <input type="textarea" name="comment" placeholder="Commentez cet article">
+    <button class="btn btn-primary">Envoyer</button>
+</form>
+<div>
+    <?php foreach($comments as $comment): ?>
+                <div class="div__chapter">
+                    <p><?= $comment->id ?></p>
+                    <h3 class="div__chapter-comment"><?= $comment->comment_author ?></h3>
+                    <p class="div__chapter-date">Paru le <?= $comment->comment_date ?></p>
+                    <p class="div__chapter-excerpt"><?= $comment->comment ?></p>
+                </div>
+    <?php endforeach; ?>
+    <?php var_dump($comment); ?>
+</div>
