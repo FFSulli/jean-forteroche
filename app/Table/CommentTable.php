93965078fa91301ffa80a2fa736e900d88f1f8de
Sullivan Berger <sullivan.berger@gmail.com>
2020-01-14 18:47:52 +0100

    Adds comments queries

--- app/Table/CommentTable.php
+++ app/Table/CommentTable.php
@@ -20,4 +20,12 @@ class CommentTable extends Table {
         return $this->query("INSERT INTO comments (post_id, comment_author, comment, comment_date) VALUES ('$post_id', '$comment_author', '$comment', NOW())");
     }
 
+    public function reportComment($comment_id) {
+        return $this->query("UPDATE comments SET report_status = 'reported' WHERE comment_id = '$comment_id'");
+    }
+
+    public function displayComments($post_id) {
+        return $this->query("SELECT comment_id, comment, comment_author, comment_date, report_status FROM posts INNER JOIN comments ON posts.id = comments.post_id WHERE post_id = ? ORDER BY comments.comment_date ASC", [$post_id]);
+    }
+
 }
