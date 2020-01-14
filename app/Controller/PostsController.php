f89635f935fb7ce36020f625488f5f28ce658347
Sullivan Berger <sullivan.berger@gmail.com>
2020-01-14 18:47:19 +0100

    Adds comments methods in the post

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -20,7 +20,6 @@ class PostsController extends AppController {
         $this->loadModel('Post');
         $this->loadModel('Comment');
 
-
     }
 
     /**
@@ -30,14 +29,21 @@ class PostsController extends AppController {
     public function show() {
 
         $article = $this->Post->find($_GET['id']);
-        $comments = $this->Post->displayComments($_GET['id']);
+        $comments = $this->Comment->displayComments($_GET['id']);
 
-        if(!empty($_POST)) {
+        if(!empty($_POST['add_comment'])) {
             $result = $this->Comment->addComment();
             if($result) {
                 header('Location: '.$_SERVER['REQUEST_URI']);
             }
-        }   
+        }  
+
+        if(isset($_POST['report_comment'])) {
+            $reported = $this->Comment->reportComment($_GET['comment_id']);
+            if($reported) {
+                header('Location: '.$_SERVER['REQUEST_URI']);
+            }
+        }
 
         $this->render('posts.show', compact('article', 'comments'));
 
