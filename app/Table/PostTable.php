36fd3e008a9082e01d78d426f34e4f1ac448b2ba
Sullivan Berger <sullivan.berger@gmail.com>
2020-01-14 18:48:42 +0100

    Moves displayComments() from PostTable to CommentTable

--- app/Table/PostTable.php
+++ app/Table/PostTable.php
@@ -30,8 +30,4 @@ class PostTable extends Table {
         return $this->query('SELECT posts.id, posts.title, posts.content, posts.excerpt, posts.date FROM posts ORDER BY posts.id DESC LIMIT 3');
     }
 
-    public function displayComments($post_id) {
-        return $this->query("SELECT id, comment, comment_author, comment_date FROM posts INNER JOIN comments ON posts.id = comments.post_id WHERE post_id = ? ORDER BY comments.comment_date ASC", [$post_id]);
-    }
-
 }
