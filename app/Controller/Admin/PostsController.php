f59391fa041fefee37c082981f33311a014d5163
“FFSulli” <sullivan.berger@gmail.com>
2020-02-06 15:51:33 +0400

    Fixes article variable issue

--- app/Controller/Admin/PostsController.php
+++ app/Controller/Admin/PostsController.php
@@ -17,6 +17,7 @@ class PostsController extends AppController{
     public function __construct(){
         parent::__construct();
         $this->loadModel('Post');
+        $this->loadModel('Comment');
     }
 
     /**
@@ -24,8 +25,10 @@ class PostsController extends AppController{
      */
 
     public function index(){
-        $posts = $this->Post->all();
-        $this->render('admin.posts.index', compact('posts'));
+        $saved_posts = $this->Post->saved();
+        $published_posts = $this->Post->published();
+        $reported_comments = $this->Comment->reportedComments();
+        $this->render('admin.posts.index', compact('saved_posts', 'published_posts', 'reported_comments'));
     }
 
     /**
@@ -33,14 +36,27 @@ class PostsController extends AppController{
      */
 
     public function add(){
-        
-        if(!empty($_POST)) {
-            $result = $this->Post->create(['title' => $_POST['title'], 'content' => $_POST['content'], 'excerpt' => $_POST['excerpt']]);
-            if($result) {
-                return $this->index();
+
+        $errors = false;
+
+        if (isset($_POST['save'])) {
+            $title = $_POST['title'];
+            $content = $_POST['content'];
+            $excerpt = $_POST['excerpt'];
+            if(empty($title)) {
+                $errors = true;
+            } elseif(empty($content)) {
+                $errors = true;
+            } elseif(empty($excerpt)) {
+                $errors = true;
+            } else {
+                $result = $this->Post->create(['title' => $_POST['title'], 'content' => $_POST['content'], 'excerpt' => $_POST['excerpt'], 'post_status' => 'saved', 'date' => date('Y-m-d H:i:s')]);
+                if($result) {
+                    return $this->index();
+                }
             }
-        }   
-        $this->render('admin.posts.edit');
+        }
+        $this->render('admin.posts.add', compact('errors'));
     }
 
     /**
@@ -56,7 +72,7 @@ class PostsController extends AppController{
             }
         }
         $post = $this->Post->find($_GET['id']);
-        $this->render('admin.posts.edit');
+        $this->render('admin.posts.edit', compact('post'));
 
     }
 
@@ -67,9 +83,43 @@ class PostsController extends AppController{
     public function delete(){
 
         if (!empty($_POST)) {
-            $result = $this->Post->delete($_POST['id']);
+            $result = $this->Post->delete($_POST['delete_post']);
             return $this->index();
 
+        }
+    }
+
+    public function preview() {
+        $preview_article = $this->Post->find($_GET['id']);
+
+        $this->render('admin.posts.preview', compact('preview_article'));
+    }
+
+    public function publish() {
+        if (!empty($_POST)) {
+            $result = $this->Post->update($_POST['publish_post'], ['post_status' => 'published']);
+            return $this->index();
+        }
+    }
+
+    public function unpublish() {
+        if (!empty($_POST)) {
+            $result = $this->Post->update($_POST['unpublish_post'], ['post_status' => 'saved']);
+            return $this->index();
+        }
+    }
+
+    public function allow_comment() {
+        if (!empty($_POST)) {
+            $this->Comment->allowComment($_POST['allow_comment']);
+            return $this->index();
+        }
+    }
+
+    public function delete_comment() {
+        if (!empty($_POST)) {
+            $result = $this->Comment->deleteComment($_POST['delete_comment']);
+            return $this->index();
         }
     }
 
